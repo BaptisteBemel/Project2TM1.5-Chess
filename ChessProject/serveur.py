8e49f3c0ad84e65836310b3cf65e102273dba4cd
BaptisteBemel <63234160+BaptisteBemel@users.noreply.github.com>
2020-12-09 12:56:09 +0100

    udp

---
+++ ChessProject/serveur.py
@@ -0,0 +1,11 @@
+import socket
+
+srv_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+srv_s.bind(('127.0.0.1', 12345))
+
+while True:
+    data, addr = srv_s.recvfrom(4096)
+    print(str(data))
+    msg = bytes('UDP serv', 'utf-8')
+    srv_s.sendto(msg, addr)
