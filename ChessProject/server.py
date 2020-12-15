7b2e83c8c4008ddad925ab45b07b1aace6461af2
BaptisteBemel <63234160+BaptisteBemel@users.noreply.github.com>
2020-12-15 15:36:51 +0100

    commit

--- ChessProject/server.py
+++ ChessProject/server.py
@@ -1,23 +1,36 @@
 # -*- coding: utf8 -*-
 import socket
 from lib.utility.util import chessboard
-# from GUI.kv import new_player
+from lib.classDir.class_file import pieces
+
 
 srv_s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 srv_s.bind((socket.gethostname(), 12345))
-msg_server = bytes(str(chessboard), "utf-8")
 
 
 while True:
     data, addr = srv_s.recvfrom(4096)
-    print("[Data Received]")
-    data = eval(data)
-    if data != chessboard:
-        chessboard = data
-    print(data)
+    chessboard_received = eval(data)
+    chessboard = chessboard_received
 
-    srv_s.sendto(msg_server, addr)
-    print("[Data send]")
+    data, addr = srv_s.recvfrom(4096)
+    isit_true_false = bool(data.decode())
 
+    if isit_true_false == True:
+        break
 
+    if pieces["king"][0].dead == 1:
+        msg_server = bytes(str(chessboard), 'utf-8')
+        true_false = bytes(str(True), 'utf-8')
+
+        srv_s.sendto(msg_server, addr)
+        srv_s.sendto(true_false, addr)
+
+    if has_played == True:
+        has_played = False
+
+        msg_server = bytes(str(chessboard), "utf-8")
+        true_false = bytes(str(False), 'utf-8')
+        srv_s.sendto(msg_server, addr)
+        srv_s.sendto(true_false, addr)
